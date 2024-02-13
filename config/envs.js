2b8eba1eb34bb1bb78ffed8c403adfc699ff910e
LUIS ROBLEDO <luis.robledo@utec.edu.pe>
2024-02-13 16:20:23 -0500

    13/2-4:20

--- config/envs.js
+++ config/envs.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 //calling .env settings
 module.exports = {
   NAME: "tmdb_users",
+  USERNAME:"tmdb_users_user",
+  PASSWORD:"UPxvAqzJC7IBc43AD9QSe8AZ5flEVPzs",
   PORT: 3001,
-  DB_HOST: "tmdb_users",
+  DB_HOST: "dpg-cn5tc57sc6pc73c78ss0-a",
   SECRET: process.env.SECRET,
   TMDB_API_KEY: process.env.API_KEY,
   TMDB_API_TOKEN: process.env.API_TOKEN,
--- db/index.js
+++ db/index.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 const config = require("../config/envs");
 
-const db = new Sequelize(config.DB_HOST, null, null, {
-  host: "localhost",
+const db = new Sequelize(config.NAME, config.USERNAME, config.PASSWORD, {
+  host: config.DB_HOST,
   dialect: "postgres",
   //logging: true,
 });
