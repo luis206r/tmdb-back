e6130aa17d7cbfd15ed685fb202619f237e1c536
LUIS ROBLEDO <luis.robledo@utec.edu.pe>
2024-02-13 17:16:08 -0500

    13/2-5:16

--- db/index.js
+++ db/index.js
@@ -1,10 +1,25 @@
 const Sequelize = require("sequelize");
 const config = require("../config/envs");
 
-const db = new Sequelize(config.NAME, config.USERNAME, config.PASSWORD, {
-  host: config.DB_HOST,
-  dialect: "postgres",
-  //logging: true,
+// const db = new Sequelize(config.NAME, config.USERNAME, config.PASSWORD, {
+//   host: config.DB_HOST,
+//   dialect: "postgres",
+//   //logging: true,
+// });
+
+const db = new Sequelize("postgres://tmdb_users_user:UPxvAqzJC7IBc43AD9QSe8AZ5flEVPzs@dpg-cn5tc57sc6pc73c78ss0-a/tmdb_users",
+"tmdb_users_user",
+"UPxvAqzJC7IBc43AD9QSe8AZ5flEVPzs",
+{
+  //host:"dpg-cn5tc57sc6pc73c78ss0-a",
+  dialect:"postgres",
 });
 
+db.authenticate()
+.then(()=>{
+  console.log("Authentication successful", db.config.database);
+})
+.catch((err)=>{
+  console.error("Failed to authenticate", err);
+})
 module.exports = db;
